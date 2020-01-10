fb37e5ab10ad48abde670b8e2f6fea738e24164c
fei5seven <usb@live.hk>
2020-01-10 23:39:21 +0800

    Update install-release.sh

--- install-release.sh
+++ install-release.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # A copy of official file: github.com/v2ray/v2ray-core/blob/master/release/install-release.sh
-# Original source is located at github.com/ColetteContreras/v2ray-poseidon/blob/master/install-release.sh
+# Original source is located at github.com/ColetteContreras/v2ray-ssrpanel-plugin/blob/master/install-release.sh
 
 # If not specify, default meaning of return value:
 # 0: Success
@@ -119,7 +119,7 @@ downloadV2Ray(){
     rm -rf /tmp/v2ray
     mkdir -p /tmp/v2ray
     colorEcho ${BLUE} "Downloading V2Ray."
-    DOWNLOAD_LINK="https://github.com/ColetteContreras/v2ray-poseidon/releases/download/${NEW_VER}/v2ray-linux-${VDIS}.zip"
+    DOWNLOAD_LINK="https://github.com/ColetteContreras/v2ray-ssrpanel-plugin/releases/download/${NEW_VER}/v2ray-linux-${VDIS}.zip"
     curl ${PROXY} -L -H "Cache-Control: no-cache" -o ${ZIPFILE} ${DOWNLOAD_LINK}
     if [ $? != 0 ];then
         colorEcho ${RED} "Failed to download! Please check your network or try again."
@@ -201,7 +201,7 @@ getVersion(){
         if [[ ${CUR_VER} != v* ]]; then
             CUR_VER=v${CUR_VER}
         fi
-        TAG_URL="https://api.github.com/repos/ColetteContreras/v2ray-poseidon/releases/latest"
+        TAG_URL="https://api.github.com/repos/ColetteContreras/v2ray-ssrpanel-plugin/releases/latest"
         NEW_VER=`curl ${PROXY} -s ${TAG_URL} --connect-timeout 10| grep 'tag_name' | head -1 | cut -d\" -f4`
         if [[ ${NEW_VER} != v* ]]; then
           NEW_VER=v${NEW_VER}
@@ -270,6 +270,7 @@ installV2Ray(){
         return 1
     fi
     makeExecutable v2ray
+    copyFile v2ctl && makeExecutable v2ctl
     copyFile geoip.dat
     copyFile geosite.dat
 
